6bddfcaceefeac46ead6b311df242ca21a737eaa
Andrew Bruner <andrewbruner@gmail.com>
2023-02-04 20:51:11 +0000

    Fix path prefix for GitHub Pages build

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,6 +1,8 @@
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 
 module.exports = function(eleventyConfig) {
+	eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 	eleventyConfig.addPlugin(syntaxHighlight);
 	eleventyConfig.addPassthroughCopy('src/css');
 	eleventyConfig.addPassthroughCopy('src/js');
