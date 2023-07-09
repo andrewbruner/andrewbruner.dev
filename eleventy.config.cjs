92dcdff491316b1183ffa319951047517c022535
Andrew Bruner <andrewbruner@gmail.com>
2023-07-09 16:24:21 -0400

    Add RSS feed for blog posts

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,8 +1,10 @@
 const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+const pluginRss = require('@11ty/eleventy-plugin-rss');
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+	eleventyConfig.addPlugin(pluginRss);
 	eleventyConfig.addPlugin(syntaxHighlight);
 	eleventyConfig.addPassthroughCopy('src/css');
 	eleventyConfig.addPassthroughCopy('src/js');
